640901f7b7f3cf05bdbc6708ee8d77c414900383
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-01-08 13:50:37 -0500

    Make header and menu

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,9 +1,38 @@
+import 'react-circular-progressbar/dist/styles.css'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar'
+import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 
 
-const Footer = () => {
+const Footer = ({ percentage=66 }) => {
+	// TODO: Olá Thiago, estou com problemas com o header e o footer tendo uma borda
+	// de 1px na direita, como resolver?
+	const navigate = useNavigate()
+
 	return (
 		<Container>
+			<button onClick={() => navigate('/habitos')}>
+				Hábitos
+			</button>
+
+			<TodayButton onClick={() => navigate('/hoje')}>
+				<CircularProgressbar
+					value={percentage}
+					text={'Hoje'}
+					background
+					backgroundPadding={6}
+					styles={buildStyles({
+						backgroundColor: '#52B6FF',
+						textColor: '#FFFFFF',
+						pathColor: '#FFFFFF',
+						trailColor: 'transparent'
+					})}
+				/>
+			</TodayButton>
+			
+			<button onClick={() => navigate('/historico')}>
+				Histórico
+			</button>
 		</Container>
 	)
 }
@@ -15,7 +44,7 @@ export {
 }
 
 
-const footerHeight = '117px'
+const footerHeight = '70px'
 
 const Container = styled.div`
 	width: 100vw;
@@ -23,4 +52,24 @@ const Container = styled.div`
 	position: absolute;
 	left: 0px;
 	bottom: 0px;
+	display: flex;
+	background-color: #FFFFFF;
+
+	> button {
+		flex-grow: 1;
+		font-size: 18px;
+		line-height: 22px;
+		text-align: center;
+		color: #52B6FF;
+	}
+`
+
+const TodayButton = styled.button`
+	width: 91px;
+	height: 91px;
+	position: absolute;
+	top: -41.5px;
+	left: calc((100vw - 91px) / 2);
+	border-radius: 50%;
+	padding: 0;
 `
